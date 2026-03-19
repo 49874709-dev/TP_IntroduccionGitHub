7f6df5023528c38ec8caef13466affd7d4e85bc5
LENA VALLINI GAYOTTO <49874709@ort>
2026-03-19 08:27:07 -0300

    guardra inicio clase Tarea

---
+++ Tarea.cs
@@ -0,0 +1,30 @@
+public class Tarea
+{
+    private int id;
+    private string descripcion; 
+    private bool completada;
+
+    public Tarea(int id, string descripcion, bool completada){
+        this.id = id;
+        this.descripcion = descripcion;
+        this.completada = completada;
+    }
+
+    public void AgregarNuevasTareas(){
+
+    }
+
+    public Tarea BuscarTraea(){
+        return ;
+
+    }
+    public void MarcarTareaComoCompleta(){
+
+    }
+    public void MostrarTodasTareas(){
+
+    }
+    public void MostrarTareasPendientes(){
+
+    }
+}
